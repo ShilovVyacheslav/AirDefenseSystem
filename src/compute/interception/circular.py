43933d7a7e60d0a9d74b2136f4bcaf302518db0a
ShilovVyacheslav <shilovvaceslav16@gmail.com>
2026-06-15 12:56:07 -0700

    add compute and utils packages

--- src/compute/interception/circular.py
+++ src/compute/interception/circular.py
@@ -1,89 +1,30 @@
 import math
-import os
-
-import pygame
-import random
 import numpy as np
 from numba import njit
-
 from scipy.special import ellipeinc
+from src.compute.ellipe.ellipe_lut import ellipe_lut
 from src.core.constants import STEP
 
-LUT_PATH = os.path.join(os.path.dirname(__file__), 'ellipe_lut.npy')
-ellipe_lut = np.load(LUT_PATH)
+
+def calculate_circular_touchdown_time(P, C, D_0, V_P, v_1, alpha_1):
+    N = D_0**2 - (C.x - P.x)**2 - (C.y - P.y)**2
+    if D_0 <= math.sqrt((C.x - P.x)**2 + (C.y - P.y)**2):
+        M_1 = (C.x - P.x) * v_1 * math.cos(alpha_1) + (C.y - P.y) * v_1 * math.sin(alpha_1) - V_P * D_0
+        t_3 = (M_1 + math.sqrt(M_1**2 - (V_P**2 - v_1**2) * N)) / (V_P**2 - v_1**2)
+    else:
+        M_2 = (C.x - P.x) * v_1 * math.cos(alpha_1) + (C.y - P.y) * v_1 * math.sin(alpha_1) + V_P * D_0
+        t_3 = (M_2 - math.sqrt(M_2**2 - (V_P**2 - v_1**2) * N)) / (V_P**2 - v_1**2)
+    return t_3
 
 
-def get_random_point(a=-10, b=10, c=-10, d=10):
-    return pygame.Vector2(random.uniform(a, b), random.uniform(c, d))
-
-
-def get_random_set(n=5, a=1.0, b=5.0):
-    return [random.uniform(a, b) for _ in range(n)]
-
-
-def calculate_spiral_time(predator, evader):
-    V_P, v_1 = predator.speed, evader.speed
-    if V_P <= v_1:
-        return float('inf')
-    D_0 = pygame.Vector2(predator.pos).distance_to(evader.C_0)
-    t_1 = D_0 / (v_1 + V_P)
-    sqrt_part = math.sqrt(V_P ** 2 - v_1 ** 2)
-    t_2pi = t_1 * math.exp((2 * math.pi * v_1) / sqrt_part)
-    return t_2pi
+def calculate_circular_revolution_time(D_0, V_P, v_1):
+    return 4 * D_0 * V_P * ellipeinc(math.pi / 2, v_1**2 / V_P**2) / (V_P**2 - v_1**2)
 
 
 def calculate_circular_time(predator, evader):
     return (calculate_circular_touchdown_time(predator.pos, evader.C_0, evader.D_0, predator.speed,
                                               evader.speed, math.atan2(evader.direction.y, evader.direction.x)) +
             calculate_circular_revolution_time(evader.D_0, predator.speed, evader.speed))
-
-
-def calculate_targeting_time(predator, evader):
-    h = evader.pos.y
-    V_P = predator.speed
-    v = evader.speed
-    if V_P <= v:
-        return float('inf')
-    return V_P * h / (V_P**2 - v**2)
-
-
-def calculate_enumeration_spiral_time(predator, evader):
-    D_0 = predator.pos.distance_to(evader.pos)
-    V_P = predator.speed
-    V_E = sorted(evader.V_E, reverse=True)
-    if V_P <= V_E[0]:
-        return float('inf')
-    m = len(V_E)
-    '''
-    t_1, t_2pi, d = [0.0] * (m + 1), [0.0] * (m + 1), [0.0] * (m + 1)
-    for k in range(1, m + 1):
-        v_k = V_E[k - 1]
-        if k == 1:
-            d[1] = D_0
-        else:
-            d[k] = (V_E[k - 2] - v_k) * t_2pi[k - 1]
-        t_1[k] = t_2pi[k - 1] + abs(d[k]) / (V_P + v_k * np.sign(d[k]))
-        t_2pi[k] = t_1[k] * math.exp((2 * math.pi * v_k) / math.sqrt(V_P**2 - v_k**2))
-    
-    if V_E == sorted(V_E, reverse=True):
-        T = (predator.exp_2pi_sum * D_0 / (V_P + V_E[m - 1]))
-    else:
-        T = (predator.exp_2pi_sum * D_0 / (V_P + V_E[0]) * math.prod([(V_P + np.sign(V_E[k] - V_E[k + 1]) * V_E[k]) /
-                                                                      (V_P + np.sign(V_E[k] - V_E[k + 1]) * V_E[k + 1])
-                                                                      for k in range(m - 1)]))
-    return T # t_2pi[m]
-    '''
-    exp_2pi_sum = math.exp(2*math.pi * sum([V_E[k] / math.sqrt(V_P**2 - V_E[k]**2) for k in range(len(V_E))]))
-    return exp_2pi_sum * D_0 / (V_P + V_E[m - 1])
-
-
-def fast_ellipeinc(val):
-    idx = val / STEP
-    i = int(idx)
-    if i >= len(ellipe_lut) - 1:
-        return ellipe_lut[-1]
-    frac = idx - i
-    return ellipe_lut[i] + frac * (ellipe_lut[i + 1] - ellipe_lut[i])
 
 
 @njit(cache=True)
@@ -151,21 +92,6 @@ def _calc_circular_core(D_0, V_P, V_E, A_E, x_C_0, y_C_0, x_P_0, y_P_0):
 def calculate_enumeration_circular_time(predator, evader):
     return _calc_circular_core(evader.D_0, predator.speed, evader.V_E, evader.A_E,
                                evader.C_0.x, evader.C_0.y, predator.pos.x, predator.pos.y)
-
-
-def calculate_circular_touchdown_time(P, C, D_0, V_P, v_1, alpha_1):
-    N = D_0**2 - (C.x - P.x)**2 - (C.y - P.y)**2
-    if D_0 <= math.sqrt((C.x - P.x)**2 + (C.y - P.y)**2):
-        M_1 = (C.x - P.x) * v_1 * math.cos(alpha_1) + (C.y - P.y) * v_1 * math.sin(alpha_1) - V_P * D_0
-        t_3 = (M_1 + math.sqrt(M_1**2 - (V_P**2 - v_1**2) * N)) / (V_P**2 - v_1**2)
-    else:
-        M_2 = (C.x - P.x) * v_1 * math.cos(alpha_1) + (C.y - P.y) * v_1 * math.sin(alpha_1) + V_P * D_0
-        t_3 = (M_2 - math.sqrt(M_2**2 - (V_P**2 - v_1**2) * N)) / (V_P**2 - v_1**2)
-    return t_3
-
-
-def calculate_circular_revolution_time(D_0, V_P, v_1):
-    return 4 * D_0 * V_P * ellipeinc(math.pi / 2, v_1**2 / V_P**2) / (V_P**2 - v_1**2)
 
 
 def compute_trajectory(D_0, V_P, v_1, gamma, t_1, theta_max=None, h=0.01):
